c98152868131648dba223837791efb55695ce491
Enjee1 <22105805+enjee1@users.noreply.github.com>
2022-03-16 16:44:09 -0400

    Initial commit

---
+++ src/main/java/com/sartorial/vocabbuilder/app/VocabBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.sartorial.vocabbuilder.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sartorial/vocabbuilder/app/VocabularyBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sartorial.vocabbuilder.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
